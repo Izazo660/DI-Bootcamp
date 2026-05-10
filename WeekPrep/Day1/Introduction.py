29375b5b88082088835264aca5553281fa4b6550
Izazo660 <i.erridaoui@gmail.com>
2026-05-10 15:16:49 +0200

    ex 1 introduction

---
+++ WeekPrep/Day1/Introduction.py
@@ -0,0 +1,52 @@
+#Exercise 1 : Boolean Logic
+
+first = "Hello World"
+
+#This is a comment
+
+print("I AM A COMPUTER")
+
+if 1 < 2 and 4 > 2:
+    print("Math is fun")
+
+nope = None
+print(nope)
+
+print(False and True)
+
+print(len("What's my length?"))
+
+print("i am shouting".upper())
+
+a = "1000"
+int(a)
+print(a)
+
+word = "4"+"real"
+print(word)
+
+print(3 * "cool")
+
+#print(1 / 0)
+
+print(type([]))
+
+name = input("What's your name ? ")
+
+number = input("Give me a number : ")
+if int(number) < 0:
+    print("That number is less than 0!")
+elif int(number) > 0:
+    print("That number is greater than 0!")
+else:
+    print("You picked 0!")
+
+word1 = "apple"
+print(word1.find("l"))
+
+word2 = "xylophone"
+print(word2.find("y"))
+
+my_string = "Hello World"
+print(my_string.islower())
+
